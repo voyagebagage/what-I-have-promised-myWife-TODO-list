9281972a4e57b58c20c76ddf1711314aa681f002
Olivier Frugier <sedatif2@MacBook-Pro-de-Olivier.local>
2021-06-29 16:21:53 +0700

    at remove animation

--- src/App.js
+++ src/App.js
@@ -9,44 +9,86 @@ library.add(faTrashAlt, faListAlt);
 function App() {
   const [list, setList] = useState([]);
   const [textInput, setTextInput] = useState("");
+  const [count, setCount] = useState(0);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // setList(textInput);
 
     const newPromiseAdd = [...list];
-    newPromiseAdd.push({
-      id: Math.round(Math.random() * 1000),
-      value: textInput,
-      haveBeenDoneYet: false,
-    });
-    setList(newPromiseAdd);
-    setTextInput("");
+
+    const arrNotDone = list.filter(
+      (elemNotDone) => elemNotDone.haveBeenDoneYet !== true
+    );
+    const arrDone = list.filter(
+      (elemDone) => elemDone.haveBeenDoneYet !== false
+    );
+
+    if (arrDone.length === 0) {
+      console.log("----1------");
+      newPromiseAdd.push({
+        id: Math.round(Math.random() * 1000),
+        value: textInput,
+        haveBeenDoneYet: false,
+        remove: false,
+      });
+      setList(newPromiseAdd);
+      setCount(list.length);
+
+      setTextInput("");
+    }
+
+    if (arrDone.length !== 0) {
+      console.log("----2------");
+      arrNotDone.push({
+        id: Math.round(Math.random() * 1000),
+        value: textInput,
+        haveBeenDoneYet: false,
+        adding: true,
+        remove: false,
+      });
+      const newArr = [...arrNotDone, ...arrDone];
+      setList(newArr);
+      setCount(list.length);
+
+      setTextInput("");
+    }
   };
+
   const handleCheck = (index) => {
     const newPromiseCheck = [...list];
-    let i;
     newPromiseCheck[index].haveBeenDoneYet =
       !newPromiseCheck[index].haveBeenDoneYet;
+    const arrNotDone = list.filter(
+      (elemNotDone) => elemNotDone.haveBeenDoneYet !== true
+    );
+    const arrDone = list.filter(
+      (elemDone) => elemDone.haveBeenDoneYet !== false
+    );
 
     if (newPromiseCheck[index].haveBeenDoneYet) {
       const itemToGoLast = newPromiseCheck.splice(index, 1);
       newPromiseCheck.push(itemToGoLast[0]);
+      return setList(newPromiseCheck);
     }
 
     if (!newPromiseCheck[index].haveBeenDoneYet) {
       const itemToGoLast = newPromiseCheck.splice(index, 1);
-      // newPromiseCheck.unshift(itemToGoLast[0]);
-      // if (newPromiseCheck[0].haveBeenDoneYet) {
-      newPromiseCheck.unshift(itemToGoLast[0]);
+      arrDone.filter((elem) => elem !== itemToGoLast[0]);
+      arrNotDone.pop();
+      const newPromiseCheckUncheck = [];
+      newPromiseCheckUncheck.push(...arrNotDone, itemToGoLast[0], ...arrDone);
+      return setList(newPromiseCheckUncheck);
     }
-    //   newPromiseCheck.splice(1, 0, itemToGoLast[0]);
-    // }
-    setList(newPromiseCheck);
   };
   const handleClickDelete = (index) => {
     const newPromiseDelete = [...list];
+    newPromiseDelete[index].remove = true;
+    console.log(newPromiseDelete[index]);
+
     newPromiseDelete.splice(index, 1);
+    setCount(list.length);
+
+    console.log(newPromiseDelete);
     setList(newPromiseDelete);
   };
 
@@ -54,23 +96,44 @@ function App() {
     <div className="container">
       <div className="list-container">
         {list.map((promise, index) => {
+          console.log("index", index);
+          console.log("count", count);
+          // setCount(list.length);
+          // if (count > list.length) {
+          //   console.log("coucouc");
+          // }
           return (
-            <ul key={promise.id}>
+            <ul
+              key={promise.id}
+              className={
+                promise.haveBeenDoneYet
+                  ? "successful-promise"
+                  : list.length < count
+                  ? "remove"
+                  : "have-not-been-done-yet"
+                // : ""
+              }
+            >
+              {console.log(
+                "---------------",
+                promise.haveBeenDoneYet,
+                "yet",
+                promise.remove,
+                "remove",
+                promise.adding,
+                "add"
+              )}
+
               <li>
                 <span>
-                  <input type="checkbox" onChange={() => handleCheck(index)} />
+                  <input
+                    type="checkbox"
+                    // className="fade-down"
+                    onChange={() => handleCheck(index)}
+                  />
                 </span>
-                <span
-                  className={
-                    promise.haveBeenDoneYet
-                      ? "successful-promise"
-                      : "have-not-been-done-yet"
-                  }
-                >
-                  &nbsp;{promise.value}&nbsp;
-                </span>
+                <span>&nbsp;{promise.value}&nbsp;</span>
                 <span onClick={() => handleClickDelete(index)}>
-                  &nbsp;&nbsp;
                   <FontAwesomeIcon icon="trash-alt" />
                 </span>
               </li>
